bug fix in the heat equation. now the numerical and exact damping depths agrees well.

py_cfe/standalonemodels/freezethaw/frozensoil.py
@@ -72,7 +72,7 @@ class FrozenSoil():
         HE.set_thermal_conductivity(TCOND)
 
         if (len(self.BC_T) > 0):
-            HE.set_boundary_conditions(self.BC_T[0])
+            HE.set_boundary_conditions(self.BC_T[0],Tbot=273.15)
         else:
             HE.set_boundary_conditions(self.Ttop)
 
@@ -117,9 +117,12 @@ class FrozenSoil():
         self.HC = np.zeros(self.NL) #volumetic heat capacity [j/m3/k]
         self.SMCT = np.zeros(self.NL)
         self.SMCLiq = np.zeros(self.NL)
-        self.ST = np.array(dat["soil_temperature"]) # initial soil temperature
-        self.Ttop = dat["surface_temperature"]
 
+        self.Ttop = dat["surface_temperature"]
+        if (dat["soil_temperature_cont"] == 273.15):
+            self.ST = np.ones(self.NL)*273.15
+        else:
+            self.ST = np.array(dat["soil_temperature"]) # initial soil temperature
         # Time step and boundary condition
         
         self.bc_change = True
@@ -135,7 +138,7 @@ class FrozenSoil():
             self.BC_T = np.ones(self.NTsteps)*260. #default
             self.Time = np.array([self.starttime + i*self.dt for i in range(self.NTsteps+1)])
             #assert (len(self.BC_T) > 0)
-            
+
     def get_Z(self):
         return self.Z
     
@@ -180,6 +183,7 @@ class FrozenSoil():
                 psi_new = self.soil_params["psi_top"]
             else:
                 psi_new = self.soil_params["psi_top"] - p1/(1000 * 9.8)
+            psi_new = z
             fact = 1 + (alpha * psi_new)**n
             theta_temp[i] = self.soil_params["theta_r"] + (self.soil_params["theta_s"] - self.soil_params["theta_r"]) * (fact**(-m))
             
@@ -429,7 +433,7 @@ class FrozenSoil():
             self.dt = (self.Time[i] - self.Time[i-1])#.seconds
 
             # set boundary conditions
-            HE.set_boundary_conditions(self.BC_T[cycles])
+            HE.set_boundary_conditions(self.BC_T[cycles],Tbot=273.15)
 
             self.ST = np.array(HE.AdvanceT(self.dt))
 

py_cfe/standalonemodels/freezethaw/heatequation.py
@@ -13,8 +13,8 @@ from numpy.linalg import inv
 # depth
 N = -1
 
-#HFUS = 2.96E6 #for saturated sandy soil
-HFUS = 0.3336E06 # latent heat of fusion (from noahmp.F)
+VHC = 2.96E6 #for saturated sandy soil
+#VHC = 0.3336E06 # latent heat of fusion (from noahmp.F)
 DF = np.zeros(1) 
 ST = np.zeros(1)
 Z = np.zeros(1)
@@ -43,7 +43,7 @@ def set_thermal_conductivity(TCond):
 #boundary conditions
 TBOT = 273.15
 TPST = 273.15
-OPT_TBOT = 1 # 1 = zero flux, 2 = pescribed temperature
+OPT_TBOT = 2 # 1 = zero flux, 2 = pescribed temperature
 
 # *************************************************************
 def set_boundary_conditions(Ttop, Tbot=-999):
@@ -70,28 +70,32 @@ def AssembleM(N, DT, ST):
     
     # compute matrix coefficient using Crank-Nicolson discretization scheme
     for i in range(0,N):
+        #print ('*******************', VHC, DF[i], i, N)
+        factor = 2000000000.0
         if i == 0:
             #h = (Z[i+1] - Z[i])
             h = Z[i]
             DTDZ  = (ST[i+1] - ST[i])/ h
-            LAMBD[i] = DT/(4* h * HFUS)
+            LAMBD[i] = factor*DT/(4* h * VHC)
             GHS = ground_flux(ST[i])
             FLUX[i] = LAMBD[i] * (DF[i] * DTDZ + GHS)
             
         elif (i < N -1):
             h = Z[i+1] - Z[i]
             h1 = Z[i] - Z[i-1]
-            LAMBD[i] = DT/(4*h* HFUS)
+            LAMBD[i] =factor* DT/(4*h* VHC)
             a_ = - LAMBD[i] * DF[i] /h1
             c_ = - LAMBD[i] * DF[i+1] /h
             b_ = 1 + a_ + c_
             FLUX[i] = -a_ * ST[i-1] + b_ * ST[i] - c_ * ST[i+1]
         elif (i == N-1):
             h = (Z[i] - Z[i-1])
-            LAMBD[i] = DT/(4* h* HFUS)
+            LAMBD[i] = factor * DT/(4* h* VHC)
             if OPT_TBOT == 1:
                 BOTFLX = 0.
+                #print ('++++++++++++++++++++++++', OPT_TBOT )
             elif OPT_TBOT == 2:
+                
                 DTDZ1 = (ST[i] - TBOT) / ( Z[i] - Z[i-1])
                 BOTFLX  = - DF[i] * DTDZ1
             DTDZ = (ST[i] - ST[i-1] )/ h
@@ -121,7 +125,8 @@ def AssembleM(N, DT, ST):
             RHS[i] = ST[i] + RHS[i]
         else:
             RHS[i] = RHS[i]
-    
+
+    X = SolverTDMA(AI, BI, CI, RHS)
     # Set up the enrite NxN matrix 
     temp_list = []
     for c in range(N):
@@ -134,24 +139,144 @@ def AssembleM(N, DT, ST):
         if i==0:
             M[i,i] = BI[i]
             M[i,i+1] = CI[i]
-        elif (i < N-1):
+        elif (i >0 and i < N-1):
             M[i,i] = BI[i]
             M[i,i+1] = CI[i]
             M[i,i-1] = AI[i]
-        else:
+        elif (i== N-1):
             M[i,i] = BI[i]
             M[i,i-1] = AI[i]
     
-    return M, RHS
+    return M, RHS, X
+
+def AssembleM_NEW(N, DT, ST):
+    FLUX = np.zeros(N)
+    AI = np.zeros(N)
+    BI = np.zeros(N)
+    CI = np.zeros(N)
+    RHS = np.zeros(N)
+    LAMBD = np.zeros(N)
+    
+    # compute matrix coefficient using Crank-Nicolson discretization scheme
+    for i in range(0,N):
+        #print ('*******************', VHC, DF[i], i, N)
+        factor = 1.0
+        if i == 0:
+            #h = (Z[i+1] - Z[i])
+            h = Z[i]
+            DTDZ  = (ST[i+1] - ST[i])/ h
+            LAMBD[i] = DT/(2* h * VHC)
+            GHS = ground_flux(ST[i])
+            FLUX[i] = LAMBD[i] * (DF[i] * DTDZ + GHS)
+        elif (i < N -1):
+            h1 = Z[i] - Z[i-1]
+            h2 = Z[i+1] - Z[i]
+            LAMBD[i] =  DT/(2*h2* VHC)
+            a_ = - LAMBD[i] * DF[i-1] /h1
+            c_ = - LAMBD[i] * DF[i] /h2
+            b_ = 1 + a_ + c_
+            FLUX[i] = -a_ * ST[i-1] + b_ * ST[i] - c_ * ST[i+1]
+        elif (i == N-1):
+            h = (Z[i] - Z[i-1])
+            LAMBD[i] =  DT/(2* h* VHC)
+            if OPT_TBOT == 1:
+                BOTFLX = 0.
+                #print ('++++++++++++++++++++++++', OPT_TBOT )
+            elif OPT_TBOT == 2:
+                DTDZ1 = (ST[i] - TBOT) / ( Z[i] - Z[i-1])
+                BOTFLX  = - DF[i] * DTDZ1
+            DTDZ = (ST[i] - ST[i-1] )/ h
+            FLUX[i]  = LAMBD[i] * (-DF[i]*DTDZ  + BOTFLX ) 
+            
+    # put coefficients in the corresponding vectors A,B,C, RHS
+    for i in range(0,N):
+        if i == 0:
+            AI[i] = 0
+            CI[i] = - LAMBD[i] *DF[i]/Z[i]
+            BI[i] = 1 - CI[i]
+        elif (i < N-1):
+            AI[i] = - LAMBD[i] * DF[i-1]/(Z[i] - Z[i-1])
+            CI[i] = - LAMBD[i] * DF[i]/(Z[i+1] - Z[i])
+            BI[i] = 1 - AI[i] - CI[i]
+        elif (i == N-1):
+            AI[i] = - LAMBD[i] * DF[i]/(Z[i] - Z[i-1])
+            CI[i] = 0
+            BI[i] = 1 - AI[i]
+        RHS[i] = FLUX[i]
+        
+    # add the previous timestep ST to the RHS at the boundaries
+    for i in range(N):
+        if i ==0:
+            RHS[i] = ST[i] + RHS[i]
+        elif i == N-1:
+            RHS[i] = ST[i] + RHS[i]
+        else:
+            RHS[i] = RHS[i]
+
+    X = SolverTDMA(AI, BI, CI, RHS)
+    # Set up the enrite NxN matrix 
+    temp_list = []
+    for c in range(N):
+        col = [0.,]*N
+        temp_list.append(list(col))
+
+    M = np.matrix(temp_list)
+
+    for i in range(N):
+        if i==0:
+            M[i,i] = BI[i]
+            M[i,i+1] = CI[i]
+        elif (i >0 and i < N-1):
+            M[i,i] = BI[i]
+            M[i,i+1] = CI[i]
+            M[i,i-1] = AI[i]
+        elif (i== N-1):
+            M[i,i] = BI[i]
+            M[i,i-1] = AI[i]
+    
+    return M, RHS, X
+
+def SolverTDMA(a, b, c, d):
+    n = len(d)
+    P = np.zeros(n)
+    Q = np.zeros(n)
+    
+    X = P
+    #Forward pass
+    denominator = b[0]
+
+    P[0] = -c[0]/denominator;
+    Q[0] =  d[0]/denominator;
+    
+    for i in range(1,n):
+        denominator = b[i] + a[i] * P[i-1];
+        
+        if (abs(denominator) < 1e-20 ):
+            return false
+        
+        P[i] =  -c[i]/denominator;
+        Q[i] = (d[i] - a[i] * Q[i-1])/denominator;
+        
+    #Backward substiution
+    X[n-1] = Q[n-1];
+    for i in range(n-2,-1,-1):
+        X[i] = P[i] * X[i+1] + Q[i]
+   
+    return X
+
 
 def UpdateT(DT, ST):
-    M, RHS = AssembleM(N, DT, ST)
+    #M, RHS, X = AssembleM(N, DT, ST)
+    M, RHS, X = AssembleM_NEW(N, DT, ST)
 
     RHS_T = np.reshape(RHS,(N,1))
     
     Minv = inv(M)
     # For CFE we will need a tridiagonal solver as well
-    Soln = np.array(Minv*RHS_T).flatten()
+    Soln = X #np.array(Minv*RHS_T).flatten()
+    #print ('X: ',X)
+    #print ('S:', Soln)
+    
     return Soln
 
 def AdvanceT(dt):